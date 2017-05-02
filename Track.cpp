31975cdb8110a8ca067bc8fad20d7b4fa0c59fa7
BeansChicken <bbyang123tw@hotmail.ca>
2017-05-02 13:39:02 -0700

    Can Track both green and red colour
    -camera frame rate at 20
    -need to
    -fine tune the the colour ranges
    -add range calculations

--- Track.cpp
+++ Track.cpp
@@ -11,27 +11,25 @@
 #define erodeSize cv::Size(3,3) //size of pixel to erode x,y
 #define dilateSize cv::Size(15, 15) //size of pixel to deilate x,y
 
-#define Blue cv::Scalar(255, 0, 0) //colour blue in RGB formate -> (Blue, Green, Red)
-#define Blue_lower_bound cv::Scalar{ 99, 139, 62 }; //colour blue lower bound in hsv formate
-#define Blue_upper_bound cv::Scalar{ 117, 255, 197 }; //colour blue upper bound in hsv formate
+#define erodeElement cv::getStructuringElement(cv::MORPH_RECT, erodeSize)
+#define dilateElement cv::getStructuringElement(cv::MORPH_RECT, dilateSize)
 
-#define Yellow cv::Scalar(0, 255, 255) //colour blue in RGB formate -> (Blue, Green, Red)
-#define Yellow_lower_bound cv::Scalar{ 0, 103, 216 }; //colour blue lower bound in hsv formate
-#define Yellow_upper_bound cv::Scalar{ 35, 162, 255 }; //colour blue upper bound in hsv formate
+#define Red cv::Scalar(0, 0, 255) //colour blue in RGB formate -> (Blue, Green, Red)
+#define Red_lower_bound cv::Scalar{ 0, 160, 247 } //colour blue lower bound in hsv formate
+#define Red_upper_bound cv::Scalar{ 12, 255, 255 } //colour blue upper bound in hsv formate
 
 #define Green cv::Scalar(0, 255, 0) //colour blue in RGB formate -> (Blue, Green, Red)
-#define Green_lower_bound cv::Scalar{ 75, 125, 158 }; //colour blue lower bound in hsv formate
-#define Green_upper_bound cv::Scalar{ 92, 255, 255 }; //colour blue upper bound in hsv formate
+#define Green_lower_bound cv::Scalar{ 49, 86, 166 } //colour blue lower bound in hsv formate
+#define Green_upper_bound cv::Scalar{ 69, 173, 199 } //colour blue upper bound in hsv formate
 
 #define White cv::Scalar(255, 255, 255) //colour blue in RGB formate -> (Blue, Green, Red)
-
 
 using namespace std;
 
 Track::Track()
 {
 #ifdef WIN4618
-	//open external cam
+	//open external cam;
 	vid.open(2);
 #endif
 
@@ -39,9 +37,6 @@ Track::Track()
 	//open external cam
 	vid.open(0);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
 #endif 
-	//Servo starting poistion
-	Control.set_data(Servo, Motor_UpDown, MotorY); 
-	Control.set_data(Servo, Motor_LeftRight, MotorX);
 }
 
 
@@ -56,106 +51,51 @@ void Track::update()
 	//start clock
 	start_tic = cv::getTickCount();
 
+	//store vid image into matrix
 	vid >> canvas;
-
-	//size of canvas
-	Image_sizeX = canvas.cols;
-	Image_sizeY = canvas.rows;
-
-	//size of canvas
-	BallPositionX = Image_sizeY / 2;
-	BallPositionY = Image_sizeX / 2;
 
 	//convert to HSV
 	cv::cvtColor(canvas, HSV, cv::COLOR_BGR2HSV);
 
 	//appy mask
-	cv::inRange(HSV, Colour_lower_bound, Colour_upper_bound, mask);
+	cv::inRange(HSV, Red_lower_bound, Red_upper_bound, canvasR);
+	cv::inRange(HSV, Green_lower_bound, Green_upper_bound, canvasG);
 
 	//kernel for erode
-	cv::Mat erodeElement = cv::getStructuringElement(cv::MORPH_RECT, erodeSize);
+	//cv::Mat erodeElement = cv::getStructuringElement(cv::MORPH_RECT, erodeSize);
 	//kernel for erode
-	cv::Mat dilateElement = cv::getStructuringElement(cv::MORPH_RECT, dilateSize);
+	//cv::Mat dilateElement = cv::getStructuringElement(cv::MORPH_RECT, dilateSize);
 
 	//erode then dilate
-	cv::erode(mask, erode, erodeElement);
-	cv::dilate(erode, dilate, dilateElement);
+	cv::erode(canvasR, canvasR, erodeElement);
+	cv::erode(canvasG, canvasG, erodeElement);
+	cv::dilate(canvasR, canvasR, dilateElement);
+	cv::dilate(canvasG, canvasG, dilateElement);
 
 	//find the shape of the object
 	vector<cv::Vec4i> hierarchy;
-	vector<vector<cv::Point>> contours;
-	findContours(dilate, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	vector<vector<cv::Point>> contoursR;
+	vector<vector<cv::Point>> contoursG;
+	findContours(canvasR, contoursR, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+	findContours(canvasG, contoursG, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
 	//draw a box around the shape, find the middle and follow it 
-	for (unsigned int i = 0; i < contours.size(); i++)
+	for (unsigned int i = 0; i < contoursR.size(); i++)
 	{
-		r = cv::boundingRect(contours.at(i));
-		BallPositionX = r.x + (r.width / 2);
-		BallPositionY = r.y + (r.height / 2);
-		string Position = to_string(BallPositionX) + ", " + to_string(BallPositionY);
+		r = cv::boundingRect(contoursR.at(i));
+
 		//display on the ball
-		cv::putText(canvas, "+", cv::Point(BallPositionX, BallPositionY), cv::FONT_HERSHEY_COMPLEX_SMALL, fontsize_fps, Colour, 3);
-		cv::putText(canvas, Position, cv::Point(BallPositionX + 10, BallPositionY + 20), cv::FONT_HERSHEY_COMPLEX_SMALL, 0.8, Colour, 1);
-		cv::rectangle(canvas, r, Colour, 3);
+		cv::rectangle(canvas, r, Red, 3);
 	}
 
-	//position calculation for Y
-	//create a y boundary (160 to 320)
-	//when ball position y is less than 160 move till it's above it (limit by motor up limit)
-	//when ball position y is more than 320 move till it's below it (limit by motor down limit)
-		if (BallPositionY < 150)
-		{
-			for (int i = 0; BallPositionY < 160; i = i + AdjustY)
-			{
-				if ((MotorY + AdjustY_Motor) < Motor_up_LIMIT)
-				{
-					MotorY = MotorY + AdjustY_Motor;
-					Control.set_data(Servo, Motor_UpDown, MotorY);
-				}
-				BallPositionY = BallPositionY + i;
-			}
-		}
-		if (BallPositionY > 330)
-		{
-			for (int i = 0; BallPositionY > 320; i = i + AdjustY)
-			{
-				if ((MotorY - AdjustY_Motor) > Motor_down_LIMIT)
-				{
-				MotorY = MotorY - AdjustY_Motor;
-				Control.set_data(Servo, Motor_UpDown, MotorY);
-				}
-				BallPositionY = BallPositionY - i;
-			}
-		}
+	for (unsigned int i = 0; i < contoursG.size(); i++)
+	{
+		r = cv::boundingRect(contoursG.at(i));
 
-		//position calculation for X
-		//create a x boundary (213 to 426)
-		//when ball position x is less than 213 move till it's above it (limit by motor up limit)
-		//when ball position x is more than 426 move till it's below it (limit by motor down limit)
-	if (BallPositionX < 213)
-	{
-		for (int i = 0; BallPositionX < 213; i = i + AdjustX)
-		{
-			if ((MotorX + AdjustX_Motor) < Motor_left_LIMIT)
-			{
-				MotorX = MotorX + AdjustX_Motor;
-				Control.set_data(Servo, Motor_LeftRight, MotorX);
-			}
-			BallPositionX = BallPositionX + i;
-		}
+		//display on the ball
+		cv::rectangle(canvas, r, Green, 3);
 	}
-	if (BallPositionX > 426)
-	{
-		for (int i = 0; BallPositionX > 426; i = i + AdjustX)
-		{
-			if ((MotorX - AdjustX_Motor) > Motor_right_LIMIT)
-			{
-				MotorX = MotorX - AdjustX_Motor;
-				Control.set_data(Servo, Motor_LeftRight, MotorX);
-			}
-			BallPositionX = BallPositionX - i;
-		}
-	}
+	
 
 	//stop clock
 	stop_tic = cv::getTickCount();
@@ -166,114 +106,13 @@ void Track::update()
 
 void Track::draw()
 {
-	if (flag_firsttime == 1)
-	{
-		//UI for choosing colour
-		cout << "Choose which color to track" << endl;
-		cout << setw(20) << "1 = Blue" << setw(20) << "2 = Yellow" << setw(20) << "3 = Green" << endl;
-		cout << "Enter a number: ";
-		cin >> flag_colour;
-
-		//check if input is within bound
-		if ((1 > flag_colour) && (flag_colour > 3))
-		{
-			cout << "Invalid choice, program will now terminate after a key press." << endl;
-			cv::waitKey(0);
-		}
-
-		//set colour choices and colour boundaries
-		if (flag_colour == 1)
-		{
-			Colour = Blue;
-			Colour_lower_bound = Blue_lower_bound;
-			Colour_upper_bound = Blue_upper_bound;
-		}
-		else if (flag_colour == 2)
-		{
-			Colour = Yellow;
-			Colour_lower_bound = Yellow_lower_bound;
-			Colour_upper_bound = Yellow_upper_bound;
-		}
-		else
-		{
-			Colour = Green;
-			Colour_lower_bound = Green_lower_bound;
-			Colour_upper_bound = Green_upper_bound;
-		}
-		flag_firsttime = 0;
-	}
-	else
-	{
 		//display fsp on canvas
 		string fsp = to_string(elapsed_time);
 		cv::putText(canvas, fsp, cv::Point(fspX, fspY), cv::FONT_HERSHEY_COMPLEX_SMALL, fontsize_fps, White, 1);
 
-		//display center of the canvas
-		cv::putText(canvas, "+", cv::Point(Image_sizeX / 2, Image_sizeY / 2), cv::FONT_HERSHEY_COMPLEX_SMALL, fontsize_fps, White, 3);
-
-		if (flag_colour == 1)
-		{
-			cv::putText(canvas, "Blue", cv::Point(fspX, fspY - 100), cv::FONT_HERSHEY_COMPLEX_SMALL, fontsize_fps, White, 1);
-		}
-		else if (flag_colour == 2)
-		{
-			cv::putText(canvas, "Yellow", cv::Point(fspX, fspY - 100), cv::FONT_HERSHEY_COMPLEX_SMALL, fontsize_fps, White, 1);
-		}
-		else
-		{
-			string fsp = to_string(elapsed_time);
-			cv::putText(canvas, "Green", cv::Point(fspX, fspY - 100), cv::FONT_HERSHEY_COMPLEX_SMALL, fontsize_fps, White, 1);
-		}
-
-		//tracking status
-		/*
-		if ( != 0)
-			cv::putText(canvas, "Tracking", cv::Point(fspX, fspY - 50), cv::FONT_HERSHEY_COMPLEX_SMALL, fontsize_fps, White, 1);
-		else
-			cv::putText(canvas, "Not Found", cv::Point(fspX, fspY - 50), cv::FONT_HERSHEY_COMPLEX_SMALL, fontsize_fps, White, 1);
-		*/
-
 		//display canvas
 		cv::imshow("Image", canvas);
 
-		//display HSV
-		//cv::imshow("HSV", HSV);
-
-		//display mask
-		cv::imshow("mask", mask);
-
-		//display erode
-		//cv::imshow("erode", erode);
-
-		//display dilate
-		//cv::imshow("dilate", dilate);
-
 		cv::waitKey(1);
-	}
 }
 
-void Track::start()
-{
-	thread t1(&Track::update_thread, this);
-	thread t2(&Track::draw_thread, this);
-	t1.join();
-	t2.join();
-}
-
-void Track::update_thread(Track* ptr)
-{
-	while (ptr->_thread_exit == false)
-	{
-		cv::waitKey(100);
-		ptr->update();
-	}
-}
-
-void Track::draw_thread(Track* ptr)
-{
-	while (ptr->_thread_exit == false)
-	{
-		cv::waitKey(100);
-		ptr->draw();
-	}
-}
--- Track.h
+++ Track.h
@@ -1,29 +1,17 @@
 #pragma once
-
-#include "Base4618.h"
-
 #include <vector>
 #include <thread>
 
+#define WIN4618 //define for running on a windows machine
+//#define PI4618 //define for running on a rasbpian machine
+
 #ifdef WIN4618
-#define Motor_down_LIMIT 10
-#define Motor_up_LIMIT 95
-#define Motor_right_LIMIT 5
-#define Motor_left_LIMIT 175
-#define Motor_xdir_MIDDLE 90
-#define Motor_UpDown 0 //channel for controingl tilt of servo motor on msp430 (p3.5)
-#define Motor_LeftRight 1	//channel for contorling pane of servo motor on msp430 (p4.1)
+#include "opencv.hpp"
 #endif
 
 #ifdef PI4618
-#define Motor_down_LIMIT 505
-#define Motor_up_LIMIT 1500
-#define Motor_right_LIMIT 505
-#define Motor_left_LIMIT 2455
-#define Motor_xdir_MIDDLE 1500
-#define Motor_UpDown 2
-#define Motor_LeftRight 3
-#endif 
+#include <opencv2/opencv.hpp>
+#endif
 
 /**
 *
@@ -35,54 +23,25 @@
 * @author Ben Yang
 *
 */
-class Track : public CBase4618
+class Track 
 {
 private:
-	int flag_firsttime = 1; //see if program as run before
-	int flag_colour = 0; //keep track of which colour was choosen
-
-	cv::Scalar Colour; //chosen colour 
-	cv::Scalar Colour_lower_bound; //chosen colour lower bound in hsv
-	cv::Scalar Colour_upper_bound; //chosen colour uppper bound in hsv
-
 	double start_tic;  //start time 
 	double stop_tic;  //stop time
 	double freq;	//freq of the system
 	double elapsed_time; //time passed (fps)
 
 	cv::VideoCapture vid; //creates the vid object for video capture
+	cv::Mat canvas; //matrix for storing video capture images for red
+	cv::Mat canvasR; //matrix for storing video capture images for red
+	cv::Mat canvasG; //matrix for storing video capture images for green
 	cv::Mat HSV;	//image after converting to HSV formate
 	cv::Mat mask;	//mask apply to the image
 	cv::Mat erode;	//image after erode
 	cv::Mat dilate; //image after dilate
 
-	int Image_sizeX; //x axis size of image
-	int Image_sizeY; //y axis size of image
 	cv::Rect r; //box drawn around the ball
-	int area = 0; //area of the box
 
-	int BallPositionX; //x coord of the ball
-	int BallPositionY; //y coord of the ball
-	int MotorX = Motor_xdir_MIDDLE; //servo x direction 
-	int MotorY = Motor_down_LIMIT; //servo y direction 
-	
-	int AdjustX = 20; //step constant in X position 
-	int AdjustY = 10; //step constant in y position 
-
-#ifdef WIN4618
-	int AdjustX_Motor = 1; //step constant for motor in X position 
-	int AdjustY_Motor = 1; //step constant for motor in y position 
-#endif
-
-#ifdef PI4618
-	int AdjustX_Motor = 10; //step constant for motor in X position 
-	int AdjustY_Motor = 10; //step constant for motor in y position 
-#endif 
-
-	//multi-threading
-	bool _thread_exit = 0; // thread safe?
-	static void update_thread(Track* ptr);
-	static void draw_thread(Track* ptr);
 public:
 
 	/** @brief Constructor
@@ -97,23 +56,16 @@ public:
 	*/
 	~Track();
 
-	/** @brief Record & process the webcam image and determine the location of the colored object.
-	*			Also, send the servo motor commands to orient the pan/tilt system to center the object in the field of view
+	/** @brief Determine the colour seen on the webcam image.
 	* @param N/A
 	* @return N/A
 	*/
 	void update();
 
 	/** @brief Display the processed image
-	* @param matrix of the image wanted to display
+	* @param N/A
 	* @return N/A
 	*/
 	void draw();
-
-	/** @brief Start the colour tracking
-	* @param matrix of the image wanted to display
-	* @return N/A
-	*/
-	void start();
 };
 
